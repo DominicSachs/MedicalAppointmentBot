50d435ab44142fd4d204aa2a9efd64b069516002
Dominic Sachs <dominic.sachs@l-p-a.com>
2018-08-31 14:58:41 +0200

    remove migration

--- src/MedicalAppointment.Common/Data/AppDbContext.cs
+++ src/MedicalAppointment.Common/Data/AppDbContext.cs
@@ -55,27 +55,66 @@ namespace MedicalAppointment.Common.Data
             builder.Entity<Appointment>().Property(p => p.AppointmentStart).IsRequired();
             builder.Entity<Appointment>().Property(p => p.AppointmentEnd).IsRequired();
             builder.Entity<Appointment>().Property(p => p.State).IsRequired();
+            
 
-            var p1 = new Patient { FirstName = "Tina", LastName = "Tester", BirthDate = DateTime.Parse("02.02.1975"), City = "Leipzig", HealthInsurance = "AOK" };
-            var p2 = new Patient { FirstName = "Sam", LastName = "Sample", BirthDate = DateTime.Parse("02.02.1982"), City = "Berlin", HealthInsurance = "KKH" };
+            
+
+            var p1 = new Patient
+            {
+                Id = 1,
+                FirstName = "Tina",
+                LastName = "Tester",
+                BirthDate = DateTime.Parse("02.02.1975"),
+                City = "Leipzig",
+                Phone = "1231",
+                HealthInsurance = "AOK",
+                CreatedBy = "Migration",
+                CreationDate = DateTime.Now,
+                ModifiedBy = "Migration",
+                ModifiedDate = DateTime.Now
+            };
+            var p2 = new Patient
+            {
+                Id = 2,
+                FirstName = "Sam",
+                LastName = "Sample",
+                BirthDate = DateTime.Parse("02.02.1982"),
+                City = "Berlin",
+                Phone = "4321",
+                HealthInsurance = "KKH",
+                CreatedBy = "Migration",
+                CreationDate = DateTime.Now,
+                ModifiedBy = "Migration",
+                ModifiedDate = DateTime.Now
+            };
 
             builder.Entity<Patient>().HasData(p1, p2);
             builder.Entity<Appointment>().HasData(
                 new Appointment
                 {
-                    Patient = p1,
+                    Id = 1,
+                    PatientId = 1,
                     AppointmentStart = DateTime.Parse("02.12.2018 12:30:00"),
                     AppointmentEnd = DateTime.Parse("02.12.2018 13:00:00"),
                     State = AppointmentState.Active,
-                    Reason = AppointmentReason.MedicalExamination
+                    Reason = AppointmentReason.MedicalExamination,
+                    CreatedBy = "Migration",
+                    CreationDate = DateTime.Now,
+                    ModifiedBy = "Migration",
+                    ModifiedDate = DateTime.Now
                 },
                 new Appointment
                 {
-                    Patient = p1,
+                    Id = 2,
+                    PatientId = 1,
                     AppointmentStart = DateTime.Parse("12.04.2019 08:00:00"),
                     AppointmentEnd = DateTime.Parse("12.04.2018 08:30:00"),
                     State = AppointmentState.Active,
-                    Reason = AppointmentReason.MedicalExamination
+                    Reason = AppointmentReason.MedicalExamination,
+                    CreatedBy = "Migration",
+                    CreationDate = DateTime.Now,
+                    ModifiedBy = "Migration",
+                    ModifiedDate = DateTime.Now
                 }
             );
         }
